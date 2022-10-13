cfeba5fe477bb61a7d80ddb489a49945cd641f74
v_zfyyang <v_zfyyang@tencent.com>
2022-10-13 14:07:18 +0800

    add config info

--- manage.py
+++ manage.py
@@ -1,12 +1,19 @@
-from flask import Flask
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
+from blog import create_app, db
 
-app = Flask(__name__)
+manager = Manager(create_app, db)
+# 数据库迁移
+Migrate(create_app, db)
+manager.add_command('db', MigrateCommand)
+
+app = create_app('development')
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route('/index')
+def index():
+    return 'index'
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=8000, debug=True)
+    app.run()
